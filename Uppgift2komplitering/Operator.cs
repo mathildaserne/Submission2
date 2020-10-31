3ac24814eec5ea43e1caa8786220301fb487326c
mathilda serneblad <mathilda.serneblad@icloud.com>
2020-10-31 14:42:55 +0100

    Skapat klassen operator Testade att skapa en array, men en lista kändes bättre

---
+++ Uppgift2komplitering/Operator.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Uppgift2komplitering
+{
+    class Operator : Program
+    {
+        /* public/private static? return?  MetodNamnet(data in) 
+           {
+              Metod kroppen!
+           }
+        */
+
+          private int Evaluate(int a, string op, int b, string op2, int c) 
+        {
+            switch (op)
+            {
+                case "+":
+                    switch (op2)
+                    {
+                        case "+":
+                            return a + b + c;
+                            
+                        case "-":
+                            return a + b - c;
+                            
+                        case "*":
+                            return a + b * c;
+                            
+                        case "/":
+                            return a + b / c;
+                            
+                        default:
+                            break;
+                    }
+                    break;
+                case "-":
+                    switch (op2)
+                    {
+                        case "+":
+                            return a - b + c;
+                            
+                        case "-":
+                            return a - b - c;
+                            
+                        case "*":
+                            return a - b * c;
+                            
+                        case "/":
+                            return a - b / c;
+                            
+                        default:
+                            break;
+                    }
+                    break;
+                case "*":
+                    switch (op2)
+                    {
+                        case "+":
+                            return a * b + c;
+                            
+                        case "-":
+                            return a * b - c;
+                            
+                        case "*":
+                            return a * b * c;
+                            
+                        case "/":
+                            return a * b / c;
+                            
+                        default:
+                            break;
+                    }
+                    break;
+                case "/":
+                    switch (op2)
+                    {
+                        case "+":
+                            return a / b + c;
+                            
+                        case "-":
+                            return a / b - c;
+                            
+                        case "*":
+                            return a / b * c;
+                            
+                        case "/":
+                            return a / b / c;
+                            
+                        default:
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+
+}
--- Uppgift2komplitering/Program.cs
+++ Uppgift2komplitering/Program.cs
@@ -1,109 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace ConsoleAppUppgift2
+namespace Uppgift2komplitering
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            int result = 0;
-
-            Console.WriteLine("Enter first number. ");
-            int Num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter first operator. ");
+            
+            List<int> Nums = new List<int>();
+            // Istället för Num1, Num2, ...
+            // Ha en Num array som håller alla värden
+            Console.WriteLine("\t\t\tEnter first number. ");
+            Nums[0] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\t\t\tEnter first operator. ");
             string Op = Console.ReadLine();
+            Console.WriteLine("\t\t\tEnter second number. ");
+            Nums[1] = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter second number. ");
-            int Num2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter second operator. ");
+            Console.WriteLine("\t\t\tEnter second operator. ");
             string Op1 = Console.ReadLine();
 
-            Console.WriteLine("Enter third number. ");
-            int Num3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\t\t\tEnter third number. ");
+            Nums[2] = Convert.ToInt32(Console.ReadLine());
 
-            switch (Op)
-            {
-                case "+":
-                    switch (Op1)
-                    {
-                        case "+":
-                            result = Num1 + Num2 + Num3;
-                            break;
-                        case "-":
-                            result = Num1 + Num2 - Num3;
-                            break;
-                        case "*":
-                            result = Num1 + Num2 * Num3;
-                            break;
-                        case "/":
-                            result = Num1 + Num2 / Num3;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case "-":
-                    switch (Op1)
-                    {
-                        case "+":
-                            result = Num1 - Num2 + Num3;
-                            break;
-                        case "-":
-                            result = Num1 - Num2 - Num3;
-                            break;
-                        case "*":
-                            result = Num1 - Num2 * Num3;
-                            break;
-                        case "/":
-                            result = Num1 - Num2 / Num3;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case "*":
-                    switch (Op1)
-                    {
-                        case "+":
-                            result = Num1 * Num2 + Num3;
-                            break;
-                        case "-":
-                            result = Num1 * Num2 - Num3;
-                            break;
-                        case "*":
-                            result = Num1 * Num2 * Num3;
-                            break;
-                        case "/":
-                            result = Num1 * Num2 / Num3;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-                case "/":
-                    switch (Op1)
-                    {
-                        case "+":
-                            result = Num1 / Num2 + Num3;
-                            break;
-                        case "-":
-                            result = Num1 / Num2 - Num3;
-                            break;
-                        case "*":
-                            result = Num1 / Num2 * Num3;
-                            break;
-                        case "/":
-                            result = Num1 / Num2 / Num3;
-                            break;
-                        default:
-                            break;
-                    }
-                    break;
-            }
 
-            Console.WriteLine(" " + Num1 + " " + Op + " " + Num2 + " " + Op1 + " " + Num3 + " = " + result);
+            int result = Operator.Evaluate (Nums[0], Op, Nums[1], Op1, Nums[2]);
+
+            Console.WriteLine(" " + Nums[0] + " " + Op + " " + Nums[1] + " " + Op1 + " " + Nums[2] + " = " + result);
             Console.WriteLine("----------------------------------------------------------------------");
             Console.WriteLine("Press any key and enter to exit");
             Console.ReadLine();
