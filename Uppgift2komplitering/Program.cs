d6661cc7cca4dc6677ba0075da480d1e1831d07e
mathilda serneblad <mathilda.serneblad@icloud.com>
2020-11-03 18:10:04 +0100

    Lagt till kommentarer. 2 nya metoder - operator creator och number reader.

--- Uppgift2komplitering/Program.cs
+++ Uppgift2komplitering/Program.cs
@@ -7,10 +7,10 @@ namespace Uppgift2komplitering
     class Program
     {
 
-        public string operator_creater()
+        public static string operator_creater()
         {
-            string op = operator_creater();
-            string op2 = operator_creater();
+            string op;
+            
             while (true)
             {
                 op = Console.ReadLine();
@@ -18,33 +18,52 @@ namespace Uppgift2komplitering
                break;
      else
                     Console.WriteLine("Please only chose +  - *   /");
+                // skapat denna metod för att endast ta emot operatorer.
             }
             return op;
         }
 
-            
+        public static int number_reader()
+        {
+            int number;
+            while (true)
+            {
+                try
+                {
+                    number = Convert.ToInt32(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please only enter integers");
+                    // skapat denna metod för att endast ta emot nummer.
+                }
+            }
+            return number;
+        }
+
 
         static void Main(string[] args)
         {
 
-            List<int> Nums = new List<int>();
-            Console.WriteLine("\t\t\tEnter first number. ");
-            Nums.Add(Convert.ToInt32(Console.ReadLine()));
-            Console.WriteLine("\t\t\tEnter first operator. ");
-            string Op = Console.ReadLine();
-            Console.WriteLine("\t\t\tEnter second number. ");
-            Nums.Add(Convert.ToInt32(Console.ReadLine()));
+            List<int> Nums = new List<int>(); // skapat en lista för att ta emot nummer.
+            Console.WriteLine("\t\t\t\tEnter first number. ");
+            Nums.Add(Program.number_reader());
+            Console.WriteLine("\t\t\t\tEnter first operator. ");
+            string Op = Program.operator_creater();
+            Console.WriteLine("\t\t\t\tEnter second number. ");
+            Nums.Add(Program.number_reader());
 
-            Console.WriteLine("\t\t\tEnter second operator. ");
-            string Op2 = Console.ReadLine();
+            Console.WriteLine("\t\t\t\tEnter second operator. ");
+            string Op2 = Program.operator_creater();
 
-            Console.WriteLine("\t\t\tEnter third number. ");
-            Nums.Add(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("\t\t\t\tEnter third number. ");
+            Nums.Add(Program.number_reader());
 
             int result = Operator.Evaluate (Nums[0], Op, Nums[1], Op2, Nums[2]);
 
             Console.WriteLine(" " + Nums[0] + " " + Op + " " + Nums[1] + " " + Op2 + " " + Nums[2] + " = " + result);
-            Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine("-------------------------------------------------------------------------");
             Console.WriteLine("Press any key and enter to exit");
             Console.ReadLine();
             Console.Clear();
